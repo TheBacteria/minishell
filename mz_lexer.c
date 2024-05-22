12a9a8615e152385213cb503140ed55fb745ccd8
Mahmoud Zouine <mzouine@e2r7p9.1337.ma>
2024-05-22 16:50:53 +0100

    auto push

--- mz_lexer.c
+++ mz_lexer.c
@@ -6,7 +6,7 @@
 /*   By: mzouine <mzouine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:08:26 by mzouine           #+#    #+#             */
-/*   Updated: 2024/05/22 16:27:10 by mzouine          ###   ########.fr       */
+/*   Updated: 2024/05/22 16:50:21 by mzouine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,47 @@
 int	make_space_nd(t_list **head, char *s, int i)
 {
 	t_list	*new;
+	char	*str;
+	int		n;
 
+	n = i;
 	while (mz_is_space(s[i]))
-	{
-		
-	}
+		i++;
+	str = ft_substr(s, n, i - n);
+	new = mz_lstnew(str);
+	free(str);
 	new->nature = 32;
+	mz_lstadd_back(head, new);
+	return (i);
+}
+
+
+int	make_word_nd(t_list **head, char *s, int i)
+{
+	t_list	*new;
+	char	*str;
+	int		n;
+
+	n = i;
+	while (!mz_is_space(s[i]) && !mz_is_special(s[i]))
+		i++;
+	str = ft_substr(s, n, i - n);
+	new = mz_lstnew(str);
+	free(str);
+	new->nature = -1;
+	mz_lstadd_back(head, new);
+	return (i);
 }
 
 int	make_special_nd(t_list **head, char *s, int i)
 {
 	t_list	*new;
+	
 
-	while (mz_is_space(s[i]))
+	if (!mz_is_special(s[i + 1]))
 	{
-		
+		new = mz_lstnew()
 	}
-}
 
-int	make_word_nd(t_list **head, char *s, int i)
-{
-	t_list	*new;
-
-	while (!mz_is_space(s[i]) && !mz_is_special(s[i]))
-	{
-		
-	}
-	new->nature = -1;
+	
 }
