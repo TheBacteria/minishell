a91ba64a526bfdfd86f533cdeb72e8b35c6c752f
Mahmoud Zouine <mzouine@e2r7p9.1337.ma>
2024-05-22 16:33:42 +0100

    auto push

--- ft_list.c
+++ ft_list.c
@@ -6,21 +6,11 @@
 /*   By: mzouine <mzouine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 16:33:34 by mzouine           #+#    #+#             */
-/*   Updated: 2024/05/22 12:14:51 by mzouine          ###   ########.fr       */
+/*   Updated: 2024/05/22 16:33:06 by mzouine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
 
 t_list	*mz_lstlast(t_list *lst)
 {
@@ -46,24 +36,6 @@ void	mz_lstadd_back(t_list **lst, t_list *new)
 	ptr->next = new;
 }
 
-char	*ft_strdup(const char *s1)
-{
-	char	*cpy;
-	size_t	i;
-
-	i = 0;
-	cpy = (char *) malloc(ft_strlen(s1) + 1);
-	if (cpy == NULL)
-		return (NULL);
-	while (s1[i])
-	{
-		cpy[i] = s1[i];
-		i++;
-	}
-	cpy[i] = '\0';
-	return (cpy);
-}
-
 t_list	*mz_lstnew(char *content)
 {
 	t_list	*new;
@@ -74,29 +46,4 @@ t_list	*mz_lstnew(char *content)
 	new -> s = ft_strdup(content);
 	new -> next = NULL;
 	return (new);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	unsigned int	i;
-	unsigned int	j;
-	char			*final;
-
-	if (!s1 && !s2)
-		return (NULL);
-	if (!s1)
-		return (ft_strdup(s2));
-	if (!s2)
-		return (ft_strdup(s1));
-	i = -1;
-	j = 0;
-	final = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!final)
-		return (NULL);
-	while (s1[++i])
-		final[i] = s1[i];
-	while (s2[j])
-		final[i++] = s2[j++];
-	final[i] = '\0';
-	return (final);
 }
--- mz_utils.c
+++ mz_utils.c
@@ -6,11 +6,39 @@
 /*   By: mzouine <mzouine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:30:52 by mzouine           #+#    #+#             */
-/*   Updated: 2024/05/22 16:31:05 by mzouine          ###   ########.fr       */
+/*   Updated: 2024/05/22 16:33:25 by mzouine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+char	*ft_strdup(const char *s1)
+{
+	char	*cpy;
+	size_t	i;
+
+	i = 0;
+	cpy = (char *) malloc(ft_strlen(s1) + 1);
+	if (cpy == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		cpy[i] = s1[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
+
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -37,4 +65,29 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	sub[i] = '\0';
 	return (sub);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	unsigned int	i;
+	unsigned int	j;
+	char			*final;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	i = -1;
+	j = 0;
+	final = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!final)
+		return (NULL);
+	while (s1[++i])
+		final[i] = s1[i];
+	while (s2[j])
+		final[i++] = s2[j++];
+	final[i] = '\0';
+	return (final);
 }
