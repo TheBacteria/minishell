b53fe4c31c9483d83ca461fe91aa6f4e28555d33
Mahmoud Zouine <mzouine@e1r8p11.1337.ma>
2024-05-22 11:40:21 +0100

    auto push

--- main.c
+++ main.c
@@ -6,44 +6,25 @@
 /*   By: mzouine <mzouine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:00:38 by mzouine           #+#    #+#             */
-/*   Updated: 2024/05/22 10:42:03 by mzouine          ###   ########.fr       */
+/*   Updated: 2024/05/22 10:51:02 by mzouine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void appah()
-{
-	system("leaks minishell");
-}
-
 int	main(int ac, char **av, char **env)
 {
 	char	*s;
-	t_list	*test;
-	t_list	*test2;
+	t_list	*envp;
 
 	if (ac != 1)
 	{
 		printf("Error!\nPlease execute the program without arguments!\n");
 		return (1);
 	}
-	test = env_parse(env);
+	envp = env_parse(env);
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	test2 = test;
-	while (test)
-	{
-		printf("%s\n", test->s);
-		test = test->next;
-	}
-	test = NULL;
-	while (test2)
-	{
-		test = test2;
-		free(test2->s);
-		test2 = test2->next;
-		free(test);
-	}
+	
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	while (1)
 	{
@@ -56,6 +37,30 @@ int	main(int ac, char **av, char **env)
 		mz_parser(s);
 		free(s);
 	}
-	atexit(appah);
 	return (0);
 }
+
+
+
+
+
+
+
+/*  !! TO FREE ENVP !!!!
+
+	t_list	*test2;
+	test2 = envp;
+	while (envp)
+	{
+		printf("%s\n", envp->s);
+		envp = envp->next;
+	}
+	envp = NULL;
+	while (test2)
+	{
+		envp = test2;
+		free(test2->s);
+		test2 = test2->next;
+		free(envp);
+	}
+*/
